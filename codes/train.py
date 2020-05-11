73fa12d864213ea10e08664584386b7a80f8071a
canhnht <canhnht1709@gmail.com>
2020-05-11 15:49:56 +0900

    Update training script

--- codes/train.py
+++ codes/train.py
@@ -16,8 +16,9 @@ from data import create_dataloader, create_dataset
 from models import create_model
 from models.modules.LPIPS import compute_dists as lpips
 
+
 def get_pytorch_ver():
-    #print(torch.__version__)
+    # print(torch.__version__)
     pytorch_ver = torch.__version__
     if pytorch_ver == "0.4.0":
         return "pre"
@@ -25,36 +26,42 @@ def get_pytorch_ver():
         return "pre"
     elif pytorch_ver == "1.0.0":
         return "pre"
-    else: #"1.1.0", "1.1.1", "1.2.0", "1.2.1" and beyond
+    else:  # "1.1.0", "1.1.1", "1.2.0", "1.2.1" and beyond
         return "post"
-        
+
+
 def main():
     # options
     parser = argparse.ArgumentParser()
-    parser.add_argument('-opt', type=str, required=True, help='Path to option JSON file.')
+    parser.add_argument('-opt', type=str, required=True,
+                        help='Path to option JSON file.')
     opt = option.parse(parser.parse_args().opt, is_train=True)
-    opt = option.dict_to_nonedict(opt)  # Convert to NoneDict, which return None for missing key.
+    # Convert to NoneDict, which return None for missing key.
+    opt = option.dict_to_nonedict(opt)
     pytorch_ver = get_pytorch_ver()
-    
+
     # train from scratch OR resume training
     if opt['path']['resume_state']:
         if os.path.isdir(opt['path']['resume_state']):
             import glob
-            resume_state_path = util.sorted_nicely(glob.glob(os.path.normpath(opt['path']['resume_state']) + '/*.state'))[-1]
+            resume_state_path = util.sorted_nicely(
+                glob.glob(os.path.normpath(opt['path']['resume_state']) + '/*.state'))[-1]
         else:
             resume_state_path = opt['path']['resume_state']
         resume_state = torch.load(resume_state_path)
     else:  # training from scratch
         resume_state = None
-        util.mkdir_and_rename(opt['path']['experiments_root'])  # rename old folder if exists
+        # rename old folder if exists
+        util.mkdir_and_rename(opt['path']['experiments_root'])
         util.mkdirs((path for key, path in opt['path'].items() if not key == 'experiments_root'
                      and 'pretrain_model' not in key and 'resume' not in key))
-    
+
     # config loggers. Before it, the log will not work
-    util.setup_logger(None, opt['path']['log'], 'train', level=logging.INFO, screen=True)
+    util.setup_logger(None, opt['path']['log'],
+                      'train', level=logging.INFO, screen=True)
     util.setup_logger('val', opt['path']['log'], 'val', level=logging.INFO)
     logger = logging.getLogger('base')
-    
+
     if resume_state:
         logger.info('Set [resume_state] to ' + resume_state_path)
         logger.info('Resuming training from epoch: {}, iter: {}.'.format(
@@ -66,9 +73,11 @@ def main():
     if opt['use_tb_logger'] and 'debug' not in opt['name']:
         from tensorboardX import SummaryWriter
         try:
-            tb_logger = SummaryWriter(logdir='../tb_logger/' + opt['name']) #for version tensorboardX >= 1.7
+            # for version tensorboardX >= 1.7
+            tb_logger = SummaryWriter(logdir='../tb_logger/' + opt['name'])
         except:
-            tb_logger = SummaryWriter(log_dir='../tb_logger/' + opt['name']) #for version tensorboardX < 1.6
+            # for version tensorboardX < 1.6
+            tb_logger = SummaryWriter(log_dir='../tb_logger/' + opt['name'])
 
     # random seed
     seed = opt['train']['manual_seed']
@@ -84,7 +93,8 @@ def main():
     for phase, dataset_opt in opt['datasets'].items():
         if phase == 'train':
             train_set = create_dataset(dataset_opt)
-            train_size = int(math.ceil(len(train_set) / dataset_opt['batch_size']))
+            train_size = int(
+                math.ceil(len(train_set) / dataset_opt['batch_size']))
             logger.info('Number of train images: {:,d}, iters: {:,d}'.format(
                 len(train_set), train_size))
             total_iters = int(opt['train']['niter'])
@@ -98,7 +108,8 @@ def main():
             logger.info('Number of val images in [{:s}]: {:d}'.format(dataset_opt['name'],
                                                                       len(val_set)))
         else:
-            raise NotImplementedError('Phase [{:s}] is not recognized.'.format(phase))
+            raise NotImplementedError(
+                'Phase [{:s}] is not recognized.'.format(phase))
     assert train_loader is not None
 
     # create model
@@ -109,26 +120,28 @@ def main():
         start_epoch = resume_state['epoch']
         current_step = resume_state['iter']
         model.resume_training(resume_state)  # handle optimizers and schedulers
-        model.update_schedulers(opt['train']) # updated schedulers in case JSON configuration has changed
+        # updated schedulers in case JSON configuration has changed
+        model.update_schedulers(opt['train'])
     else:
         current_step = 0
         start_epoch = 0
 
     # training
-    logger.info('Start training from epoch: {:d}, iter: {:d}'.format(start_epoch, current_step))
+    logger.info('Start training from epoch: {:d}, iter: {:d}'.format(
+        start_epoch, current_step))
     for epoch in range(start_epoch, total_epochs):
-        for n, train_data in enumerate(train_loader,start=1):
+        for n, train_data in enumerate(train_loader, start=1):
             current_step += 1
             if current_step > total_iters:
                 break
-            
-            if pytorch_ver=="pre": #Order for PyTorch ver < 1.1.0
+
+            if pytorch_ver == "pre":  # Order for PyTorch ver < 1.1.0
                 # update learning rate
                 model.update_learning_rate(current_step-1)
                 # training
                 model.feed_data(train_data)
                 model.optimize_parameters(current_step)
-            elif pytorch_ver=="post": #Order for PyTorch ver > 1.1.0
+            elif pytorch_ver == "post":  # Order for PyTorch ver > 1.1.0
                 # training
                 model.feed_data(train_data)
                 model.optimize_parameters(current_step)
@@ -153,9 +166,10 @@ def main():
             # save models and training states (changed to save models before validation)
             if current_step % opt['logger']['save_checkpoint_freq'] == 0:
                 model.save(current_step)
-                model.save_training_state(epoch + (n >= len(train_loader)), current_step)
+                model.save_training_state(
+                    epoch + (n >= len(train_loader)), current_step)
                 logger.info('Models and training states saved.')
-            
+
             # validation
             if current_step % opt['train']['val_freq'] == 0:
                 avg_psnr = 0.0
@@ -166,7 +180,8 @@ def main():
                 val_gt_imgs_list = []
                 for val_data in val_loader:
                     idx += 1
-                    img_name = os.path.splitext(os.path.basename(val_data['LR_path'][0]))[0]
+                    img_name = os.path.splitext(
+                        os.path.basename(val_data['LR_path'][0]))[0]
                     img_dir = os.path.join(opt['path']['val_images'], img_name)
                     util.mkdir(img_dir)
 
@@ -174,25 +189,27 @@ def main():
                     model.test()
 
                     visuals = model.get_current_visuals()
-                    
-                    if opt['datasets']['train']['znorm']: # If the image range is [-1,1]
-                        sr_img = util.tensor2img(visuals['SR'],min_max=(-1, 1))  # uint8
-                        gt_img = util.tensor2img(visuals['HR'],min_max=(-1, 1))  # uint8
-                    else: # Default: Image range is [0,1]
+
+                    if opt['datasets']['train']['znorm']:  # If the image range is [-1,1]
+                        sr_img = util.tensor2img(
+                            visuals['SR'], min_max=(-1, 1))  # uint8
+                        gt_img = util.tensor2img(
+                            visuals['HR'], min_max=(-1, 1))  # uint8
+                    else:  # Default: Image range is [0,1]
                         sr_img = util.tensor2img(visuals['SR'])  # uint8
                         gt_img = util.tensor2img(visuals['HR'])  # uint8
-                        
+
                     # sr_img = util.tensor2img(visuals['SR'])  # uint8
                     # gt_img = util.tensor2img(visuals['HR'])  # uint8
-                    
+
                     # print("Min. SR value:",sr_img.min()) # Debug
                     # print("Max. SR value:",sr_img.max()) # Debug
-                    
+
                     # print("Min. GT value:",gt_img.min()) # Debug
                     # print("Max. GT value:",gt_img.max()) # Debug
-                    
+
                     # Save SR images for reference
-                    save_img_path = os.path.join(img_dir, '{:s}_{:d}.png'.format(\
+                    save_img_path = os.path.join(img_dir, '{:s}_{:d}.png'.format(
                         img_name, current_step))
                     util.save_img(sr_img, save_img_path)
 
@@ -203,33 +220,44 @@ def main():
 
                     # For training models with only one channel ndim==2, if RGB ndim==3, etc.
                     if gt_img.ndim == 2:
-                        cropped_gt_img = gt_img[crop_size:-crop_size, crop_size:-crop_size]
+                        cropped_gt_img = gt_img[crop_size:-
+                                                crop_size, crop_size:-crop_size]
                     else:
-                        cropped_gt_img = gt_img[crop_size:-crop_size, crop_size:-crop_size, :]
+                        cropped_gt_img = gt_img[crop_size:-
+                                                crop_size, crop_size:-crop_size, :]
                     if sr_img.ndim == 2:
-                        cropped_sr_img = sr_img[crop_size:-crop_size, crop_size:-crop_size]
-                    else: # Default: RGB images
-                        cropped_sr_img = sr_img[crop_size:-crop_size, crop_size:-crop_size, :]
-                    
-                    val_gt_imgs_list.append(cropped_gt_img) # If calculating only once for all images
-                    val_sr_imgs_list.append(cropped_sr_img) # If calculating only once for all images
-                    
+                        cropped_sr_img = sr_img[crop_size:-
+                                                crop_size, crop_size:-crop_size]
+                    else:  # Default: RGB images
+                        cropped_sr_img = sr_img[crop_size:-
+                                                crop_size, crop_size:-crop_size, :]
+
+                    # If calculating only once for all images
+                    val_gt_imgs_list.append(cropped_gt_img)
+                    # If calculating only once for all images
+                    val_sr_imgs_list.append(cropped_sr_img)
+
                     # LPIPS only works for RGB images
-                    avg_psnr += util.calculate_psnr(cropped_sr_img * 255, cropped_gt_img * 255)
-                    avg_ssim += util.calculate_ssim(cropped_sr_img * 255, cropped_gt_img * 255)
-                    #avg_lpips += lpips.calculate_lpips([cropped_sr_img], [cropped_gt_img]) # If calculating for each image
+                    avg_psnr += util.calculate_psnr(
+                        cropped_sr_img * 255, cropped_gt_img * 255)
+                    avg_ssim += util.calculate_ssim(
+                        cropped_sr_img * 255, cropped_gt_img * 255)
+                    # If calculating for each image
+                    avg_lpips += lpips.calculate_lpips(
+                        [cropped_sr_img], [cropped_gt_img])
 
                 avg_psnr = avg_psnr / idx
                 avg_ssim = avg_ssim / idx
-                #avg_lpips = avg_lpips / idx # If calculating for each image
-                avg_lpips = lpips.calculate_lpips(val_sr_imgs_list,val_gt_imgs_list) # If calculating only once for all images
+                avg_lpips = avg_lpips / idx  # If calculating for each image
+                # avg_lpips = lpips.calculate_lpips(val_sr_imgs_list,val_gt_imgs_list) # If calculating only once for all images
 
                 # log
                 # logger.info('# Validation # PSNR: {:.5g}, SSIM: {:.5g}'.format(avg_psnr, avg_ssim))
-                logger.info('# Validation # PSNR: {:.5g}, SSIM: {:.5g}, LPIPS: {:.5g}'.format(avg_psnr, avg_ssim, avg_lpips))
+                logger.info('# Validation # PSNR: {:.5g}, SSIM: {:.5g}, LPIPS: {:.5g}'.format(
+                    avg_psnr, avg_ssim, avg_lpips))
                 logger_val = logging.getLogger('val')  # validation logger
                 # logger_val.info('<epoch:{:3d}, iter:{:8,d}> psnr: {:.5g}, ssim: {:.5g}'.format(
-                    # epoch, current_step, avg_psnr, avg_ssim))
+                # epoch, current_step, avg_psnr, avg_ssim))
                 logger_val.info('<epoch:{:3d}, iter:{:8,d}> psnr: {:.5g}, ssim: {:.5g}, lpips: {:.5g}'.format(
                     epoch, current_step, avg_psnr, avg_ssim, avg_lpips))
                 # tensorboard logger
